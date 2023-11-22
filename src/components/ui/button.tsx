1aefef52c40afbf0eb4b4bb7c93e18e3f00a2bfd
bscarpari <brunoomoraes686@gmail.com>
2023-11-22 17:11:54 -0300

    fix: removing theme toggle

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "src/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 uppercase",
   {
     variants: {
       variant: {
-        gradient: "bg-gradient text-primary opacity-90 hover:opacity-80 uppercase",
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 uppercase",
+        gradient: "bg-gradient text-primary-foreground opacity-90 hover:opacity-80",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
