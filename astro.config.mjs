23af98c496607a8b2ca1e101d03863eccf840fcc
bscarpari <brunoomoraes686@gmail.com>
2023-11-22 16:23:37 -0300

    chore: adding analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,20 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    robotsTxt(),
-    react(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), robotsTxt(), react()],
   site: SITE_URL,
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
